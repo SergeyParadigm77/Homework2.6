7fc395b2051e52594d7b392cbcb141c5f36bc900
SergeyParadigm77 <paradigm77@rambler.ru>
2023-04-02 22:41:13 +0300

    ДЗ выполнено!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        List<String> words = new ArrayList<>(List.of("my","book", "is", "my", "book", "forever"));
+        ServiceImpl serviceImpl = new ServiceImpl();
+        serviceImpl.printOnlyOdd(nums);
+        System.out.println();
+        serviceImpl.printOnlyEvenWithoutDoubles(nums);
+        System.out.println();
+        serviceImpl.printOnlyUniqueWords(words);
+        serviceImpl.printNumberOfRepetitions(words);
     }
 }
